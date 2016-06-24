93e4d8fd66be3b93b1092a348ceeabd015f6a96f
Mathieu Poumeyrol <kali@zoy.org>
2016-06-24 15:43:32 +0200

    use relative path wherever it makes sense

---
+++ android-linker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+$HOME/Library/Android/sdk/ndk-bundle/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-gcc --sysroot $HOME/Library/Android/sdk/ndk-bundle/platforms/android-18/arch-arm "$@"
--- custom.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/Users/kali/Library/Android/sdk/ndk-bundle/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-gcc --sysroot /Users/kali/Library/Android/sdk/ndk-bundle/platforms/android-18/arch-arm "$@"
